8a8c8f8f2ca1e59821454a2a3d0ed750a7973f43
Nerya Sheetrit <neryasheetrit@gmail.com>
2022-02-26 20:18:26 +0200

    verfication and other cool stuff

--- public/game.js
+++ public/game.js
@@ -1,7 +1,9 @@
 var guess = "";
-var len;
+var LENGTH = 10;
 var attempts = 0;
-
+var letterAt = 0;
+const currWord = "g23ty25543"
+// const currWord = "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824";
 function setMyKeyDownListener() {
     window.addEventListener(
       "keydown",
@@ -10,23 +12,45 @@ function setMyKeyDownListener() {
 }
 
 function handleKeydown(eventKey) {
-    if (eventKey == 13) { // In case of enter
-        if (guess.length == len)
-            makeAttempt(guess);
-    }
-    else
-        guess += String.fromCharCode(eventKey);
-}
+  if(true){
+    if (eventKey === "Enter") { // In case of enter
+      if (guess.length == LENGTH){
 
-function new_game() {
-    var seed = "";
-    len = Math.floor(Math.random() * 40 + 20);
-    for (var i = 0; i < len; i++) {
-        seed += String.fromCharCode(Math.floor(Math.random() * 100));
+        makeAttempt(guess);
+      }
+    }else if(eventKey === "Backspace" && letterAt > 0){
+      document.querySelector(".txt" + (attempts+"") + ((letterAt-1)+"")).innerHTML = "";
+      guess=guess.substring(0,guess.length-1);
+      letterAt--;
+    }else{
+      console.log(eventKey);
+      if(eventKey.trim().length == 1 && guess.length < LENGTH){
+        guess += (eventKey);
+        document.querySelector(".txt" + (attempts+"") + (letterAt+"")).innerHTML = eventKey;
+        letterAt++;
+      }
     }
-  
-    var code = sha256(seed);
+
+  }
+}
+const makeAttempt = (guess) => {
+  console.log(guess);
+  for(let i = 0; i < LENGTH; i++){
+    if(guess[i] == currWord[i]){
+      document.querySelector(".cube" + (attempts+"") + (i+"")).className = ".cube" + (attempts+"") + (i+"") + " cube success";
+    }
+  }
+}
+newGame()
+function newGame() {
+    // var seed = "";
+    // len = Math.floor(Math.random() * 40 + 20);
+    // for (var i = 0; i < len; i++) {
+    //     seed += String.fromCharCode(Math.floor(Math.random() * 100));
+    // }
+    //
+    // var code = sha256(seed);
 
     setMyKeyDownListener();
- 
+
 }
--- public/script.js
+++ public/script.js
@@ -7,11 +7,14 @@ function generateBoard(rows,cells){
     row.className = "row";
     for(var i = 0; i < cells; i++){
       let cube = document.createElement("div");
-      cube.className = "cube";
+      cube.className = "cube cube" + (j + "") + (i + "");
+      let cubeTxt = document.createElement("p");
+      cubeTxt.className = "cubeTXT txt" + (j + "") + (i + "");
+      cube.append(cubeTxt);
       row.append(cube);
     }
     document.querySelector(".contAll").append(row);
   }
 
 }
-generateBoard(ATTEMPTS,32)
+generateBoard(ATTEMPTS,LENGTH)
